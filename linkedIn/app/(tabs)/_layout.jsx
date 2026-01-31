b9661a1df10033052632000ed0c912557e3b72a3
C_Jenkins <gbengachris87@gmail.com>
2026-01-31 15:35:08 +0100

    enhance tab layout with color customization and remove unused network screen

--- linkedIn/app/(tabs)/_layout.jsx
+++ linkedIn/app/(tabs)/_layout.jsx
@@ -4,27 +4,35 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 
 const TabsLayout = () => {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "cyan",
+        tabBarInactiveTintColor: "gray",
+      }}
+    >
       <Tabs.Screen
         name="home"
         options={{
           title: "Home",
-          tabBarIcon: () => <Entypo name="home" size={24} color="black" />,
+          tabBarIcon: ({ color }) => (
+            <Entypo name="home" size={24} color={color} />
+          ),
         }}
       />
-      <Tabs.Screen
+      {/* <Tabs.Screen
         name="network"
         options={{
           title: "Network",
           tabBarIcon: () => <Entypo name="users" size={24} color="black" />,
         }}
-      />
+      /> */}
       <Tabs.Screen
         name="post"
         options={{
           title: "Post",
-          tabBarIcon: () => (
-            <Ionicons name="duplicate" size={24} color="black" />
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="duplicate" size={24} color={color} />
           ),
         }}
       />
@@ -32,8 +40,8 @@ const TabsLayout = () => {
         name="notifications"
         options={{
           title: "Notifications",
-          tabBarIcon: () => (
-            <Ionicons name="notifications" size={24} color="black" />
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="notifications" size={24} color={color} />
           ),
         }}
       />
@@ -41,8 +49,8 @@ const TabsLayout = () => {
         name="jobs"
         options={{
           title: "Jobs",
-          tabBarIcon: () => (
-            <Ionicons name="briefcase" size={24} color="black" />
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="briefcase" size={24} color={color} />
           ),
         }}
       />
--- linkedIn/app/_layout.jsx
+++ linkedIn/app/_layout.jsx
@@ -1,13 +1,14 @@
 import { Stack } from "expo-router";
 
 const RootLayout = () => {
-
   return (
     <Stack screenOptions={{ headerShown: false }}>
+      {/* Explicitly define the index screen first */}
+      <Stack.Screen name="index" />
       <Stack.Screen name="(tabs)" />
+
     </Stack>
   );
 };
 
 export default RootLayout;
- 
