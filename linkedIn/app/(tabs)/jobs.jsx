dcd15017c3bf4e557a6e63edf7e83b06fdcc22a4
C_Jenkins <gbengachris87@gmail.com>
2026-01-31 17:00:33 +0100

    `Added Ionicons import and home button to Jobs screen`

--- linkedIn/app/(tabs)/jobs.jsx
+++ linkedIn/app/(tabs)/jobs.jsx
@@ -1,11 +1,27 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { router } from "expo-router";
 
 const Jobs = () => {
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <View>
-        <Text style={{fontSize: 30, textAlign: 'center'}}>Welcome to the Jobs screen</Text>
+    <SafeAreaView
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <Text style={{ fontSize: 30, textAlign: "center" }}>
+          Welcome to the Jobs screen
+        </Text>
+
+        <Ionicons
+          name="home"
+          size={50}
+          color="black"
+          style={{ marginTop: 20 }}
+          onPress={() => {
+            router.back();
+          }}
+        />
       </View>
     </SafeAreaView>
   );
